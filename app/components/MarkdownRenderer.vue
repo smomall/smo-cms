ef7d72c493ef5a10dfe41cbf6b0893c68f0638f8
kongdechang <zvxf@outlook.com>
2026-08-31 18:11:50 +0800

    update

--- app/components/MarkdownRenderer.vue
+++ app/components/MarkdownRenderer.vue
@@ -9,69 +9,69 @@
 </template>
 
 <script setup lang="ts">
-import shiki from '@comark/nuxt/plugins/shiki'
-import math from '@comark/nuxt/plugins/math'
-import 'katex/dist/katex.min.css'
-import binding from '@comark/nuxt/plugins/binding'
-import breaks from '@comark/nuxt/plugins/breaks'
-import emoji from '@comark/nuxt/plugins/emoji'
-import footnotes from '@comark/nuxt/plugins/footnotes'
-import headings from '@comark/nuxt/plugins/headings'
-import punctuation from '@comark/nuxt/plugins/punctuation'
-import security from '@comark/nuxt/plugins/security'
-import mermaid from 'mermaid'
-import light from '@shikijs/themes/catppuccin-mocha'
+import shiki from "@comark/nuxt/plugins/shiki";
+import math from "@comark/nuxt/plugins/math";
+import "katex/dist/katex.min.css";
+import binding from "@comark/nuxt/plugins/binding";
+import breaks from "@comark/nuxt/plugins/breaks";
+import emoji from "@comark/nuxt/plugins/emoji";
+import footnotes from "@comark/nuxt/plugins/footnotes";
+import headings from "@comark/nuxt/plugins/headings";
+import punctuation from "@comark/nuxt/plugins/punctuation";
+import security from "@comark/nuxt/plugins/security";
+import mermaid from "mermaid";
+import light from "@shikijs/themes/catppuccin-mocha";
 // 高亮
-import astro from '@shikijs/langs/astro'
-import blade from '@shikijs/langs/blade'
-import c from '@shikijs/langs/c'
-import coffeescript from '@shikijs/langs/coffeescript'
-import cpp from '@shikijs/langs/cpp'
-import css from '@shikijs/langs/css'
-import glsl from '@shikijs/langs/glsl'
-import graphql from '@shikijs/langs/graphql'
-import haml from '@shikijs/langs/haml'
-import handlebars from '@shikijs/langs/handlebars'
-import html from '@shikijs/langs/html'
-import http from '@shikijs/langs/http'
-import imba from '@shikijs/langs/imba'
-import java from '@shikijs/langs/java'
-import javascript from '@shikijs/langs/javascript'
-import jinja from '@shikijs/langs/jinja'
-import jison from '@shikijs/langs/jison'
-import json from '@shikijs/langs/json'
-import json5 from '@shikijs/langs/json5'
-import jsonc from '@shikijs/langs/jsonc'
-import jsonl from '@shikijs/langs/jsonl'
-import jsx from '@shikijs/langs/jsx'
-import julia from '@shikijs/langs/julia'
-import less from '@shikijs/langs/less'
-import lua from '@shikijs/langs/lua'
-import markdown from '@shikijs/langs/markdown'
-import marko from '@shikijs/langs/marko'
-import mdc from '@shikijs/langs/mdc'
-import mdx from '@shikijs/langs/mdx'
-import php from '@shikijs/langs/php'
-import postcss from '@shikijs/langs/postcss'
-import pug from '@shikijs/langs/pug'
-import python from '@shikijs/langs/python'
-import r from '@shikijs/langs/r'
-import ruby from '@shikijs/langs/ruby'
-import sass from '@shikijs/langs/sass'
-import scss from '@shikijs/langs/scss'
-import shell from '@shikijs/langs/shell'
-import sql from '@shikijs/langs/sql'
-import stylus from '@shikijs/langs/stylus'
-import svelte from '@shikijs/langs/svelte'
-import toml from '@shikijs/langs/toml'
-import tsx from '@shikijs/langs/tsx'
-import typescript from '@shikijs/langs/typescript'
-import vue from '@shikijs/langs/vue'
-import vueHtml from '@shikijs/langs/vue-html'
-import wasm from '@shikijs/langs/wasm'
-import wgsl from '@shikijs/langs/wgsl'
-import xml from '@shikijs/langs/xml'
-import yaml from '@shikijs/langs/yaml'
+import astro from "@shikijs/langs/astro";
+import blade from "@shikijs/langs/blade";
+import c from "@shikijs/langs/c";
+import coffeescript from "@shikijs/langs/coffeescript";
+import cpp from "@shikijs/langs/cpp";
+import css from "@shikijs/langs/css";
+import glsl from "@shikijs/langs/glsl";
+import graphql from "@shikijs/langs/graphql";
+import haml from "@shikijs/langs/haml";
+import handlebars from "@shikijs/langs/handlebars";
+import html from "@shikijs/langs/html";
+import http from "@shikijs/langs/http";
+import imba from "@shikijs/langs/imba";
+import java from "@shikijs/langs/java";
+import javascript from "@shikijs/langs/javascript";
+import jinja from "@shikijs/langs/jinja";
+import jison from "@shikijs/langs/jison";
+import json from "@shikijs/langs/json";
+import json5 from "@shikijs/langs/json5";
+import jsonc from "@shikijs/langs/jsonc";
+import jsonl from "@shikijs/langs/jsonl";
+import jsx from "@shikijs/langs/jsx";
+import julia from "@shikijs/langs/julia";
+import less from "@shikijs/langs/less";
+import lua from "@shikijs/langs/lua";
+import markdown from "@shikijs/langs/markdown";
+import marko from "@shikijs/langs/marko";
+import mdc from "@shikijs/langs/mdc";
+import mdx from "@shikijs/langs/mdx";
+import php from "@shikijs/langs/php";
+import postcss from "@shikijs/langs/postcss";
+import pug from "@shikijs/langs/pug";
+import python from "@shikijs/langs/python";
+import r from "@shikijs/langs/r";
+import ruby from "@shikijs/langs/ruby";
+import sass from "@shikijs/langs/sass";
+import scss from "@shikijs/langs/scss";
+import shell from "@shikijs/langs/shell";
+import sql from "@shikijs/langs/sql";
+import stylus from "@shikijs/langs/stylus";
+import svelte from "@shikijs/langs/svelte";
+import toml from "@shikijs/langs/toml";
+import tsx from "@shikijs/langs/tsx";
+import typescript from "@shikijs/langs/typescript";
+import vue from "@shikijs/langs/vue";
+import vueHtml from "@shikijs/langs/vue-html";
+import wasm from "@shikijs/langs/wasm";
+import wgsl from "@shikijs/langs/wgsl";
+import xml from "@shikijs/langs/xml";
+import yaml from "@shikijs/langs/yaml";
 
 const langs = [
   astro,
@@ -123,16 +123,16 @@ const langs = [
   wasm,
   wgsl,
   xml,
-  yaml
-]
+  yaml,
+];
 
 interface Props {
-  content?: string
+  content?: string;
 }
 
 withDefaults(defineProps<Props>(), {
-  content: ''
-})
+  content: "",
+});
 const plugins = [
   shiki({
     preStyles: true,
@@ -140,7 +140,7 @@ const plugins = [
     themes: {
       light: light,
       // dark: dark
-    }
+    },
   }),
   math(),
   binding(),
@@ -149,14 +149,29 @@ const plugins = [
   footnotes(),
   headings(),
   punctuation(),
-  security()
-]
+  security(),
+];
 
 onMounted(async () => {
-  mermaid.initialize({ startOnLoad: false })
+  mermaid.initialize({ startOnLoad: false });
   await mermaid.run({
-    querySelector: '.language-mermaid',
-    suppressErrors: true
-  })
-})
+    querySelector: ".language-mermaid",
+    suppressErrors: true,
+  });
+});
 </script>
+<style scoped>
+.markdown-body h1,
+.markdown-body h2,
+.markdown-body h3,
+.markdown-body h4,
+.markdown-body h5,
+.markdown-body h6 {
+  /* 长无空格标识符自动换行 */
+  overflow-wrap: break-word;
+  /* 标题均衡换行排版 */
+  text-wrap: balance;
+  /* flex父容器换行兜底修复 */
+  min-width: 0;
+}
+</style>
